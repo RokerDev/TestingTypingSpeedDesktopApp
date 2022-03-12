6188080b1df7db0fbeb10260d02c126ccdb5f529
RokerDev <rokerdev91@gmail.com>
2022-03-12 16:59:20 +0100

    feat: add get_user_entry and logic to display
    As the user enters characters and words, the function will start counting them. It also removes the words from the label, if they were typed correctly.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from tkinter import ttk
 
 
 # To do list:
-# TODO: add text generator
 # TODO: display generated text on label
 # TODO: get user input
 # TODO: check user input with generated text
@@ -22,21 +21,27 @@ class TypeSpeedTest(tk.Tk):
         self.title("Type Speed Test Desktop Application")
         self.geometry(f"{int(self.winfo_screenwidth() / 2)}x{int(self.winfo_screenheight() / 4)}")
 
-        self.lbl_display_list = ttk.Label(self, text="Cos", relief=tk.SUNKEN, font=("Courier", 60))
+        self.lbl_display_list = ttk.Label(self, text="Cos", relief=tk.SUNKEN, font=("Courier", 40))
         self.lbl_display_list.grid(column=0, row=0, padx=20, pady=10, sticky=tk.EW)
         self.columnconfigure(0, minsize=100, weight=1)
         self.rowconfigure(0, minsize=80, weight=1)
 
-        self.ent_text = ttk.Entry(self, font=("Courier", 40))
+        self.text_str_var = tk.StringVar()
+        self.text_str_var.trace("w", self.get_user_entry)
+
+        self.ent_text = ttk.Entry(self, font=("Courier", 40), textvariable=self.text_str_var, )
         self.ent_text.grid(column=0, row=1, padx=20, pady=10, sticky=tk.EW)
         self.rowconfigure(1, minsize=60, weight=1, )
 
         # Backend
         self.words_list = self.load_words_list()
         self.generated_words_string = ""
-        self.generate_world()
-        self.generate_world()
-        self.generate_world()
+        self.words_per_minute = 0
+        self.chars_per_minute = 0
+        for _ in range(6):
+            self.generate_word()
+
+        self.lbl_display_list.config(text=self.generated_words_string)
 
     @staticmethod
     def load_words_list():
@@ -50,6 +55,23 @@ class TypeSpeedTest(tk.Tk):
             self.generated_words_string += generated_word
         else:
             self.generated_words_string += f" {generated_word}"
+
+    def get_user_entry(self, *args):
+        text = self.text_str_var.get()
+
+        if text != self.generated_words_string[:len(text)]:
+            self.text_str_var.set(text[:-1])
+        else:
+            self.chars_per_minute += 1
+            if text.endswith(" "):
+                self.generate_word()
+                self.generated_words_string = self.generated_words_string[len(text):]
+                self.lbl_display_list["text"] = self.generated_words_string
+                self.text_str_var.set("")
+                self.words_per_minute += 1
+
+        print(f"WPM: {self.words_per_minute}. CPM: {self.chars_per_minute}.")
+
 
 
 if __name__ == "__main__":
